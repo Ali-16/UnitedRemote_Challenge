f3f8ec374280aada2ffdb8d194a81ca37d2220a0
ali-16 <ali.sidibaba.pro@gmail.com>
2019-11-26 14:55:36 +0100

    Setup the front-end part with main features, components and services

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,10 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthComponent } from './Authentification/auth/auth.component';
+import { SigninComponent } from './Authentification/Auth/signin/signin.component';
+import { SignupComponent } from './Authentification/Auth/signup/signup.component';
+import { NavbarComponent } from './header/navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ShopsListComponent } from './shops/shops-list/shops-list.component';
+import { ShopsItemComponent } from './shops/shops-list/shops-item/shops-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    SigninComponent,
+    SignupComponent,
+    NavbarComponent,
+    FooterComponent,
+    ShopsListComponent,
+    ShopsItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/shops/shops-list/shops-item/shops-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shops-item',
+  templateUrl: './shops-item.component.html',
+  styleUrls: ['./shops-item.component.css']
+})
+export class ShopsItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
