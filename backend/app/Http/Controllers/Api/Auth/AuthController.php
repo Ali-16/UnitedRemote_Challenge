7bd4770d5d435b873ffb4517f4c4778f0954ad56
ali-16 <ali.sidibaba.pro@gmail.com>
2019-11-29 23:39:00 +0100

    Initialize and configurate a jwt-base authentication system
    	- Install tymondesigns/jwt-auth package.
    	- Add and config AuthController and User model
    	- Install barryvdh/laravel-cors package to resolve CORS blocking issue

---
+++ backend/app/Http/Controllers/Api/Auth/AuthController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\SignUpRequest;
+use App\User;
+
+
+
+class AuthController extends Controller
+{
+    /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'signup']]);
+    }
+
+    /**
+     * Tries to log a user in and get a JWT.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function signIn()
+    {
+        $credentials = request(['email', 'password']);
+
+        if (!$token = auth()->attempt($credentials)) {
+            return response()->json(['error' => "Email or Password doesn't exist"], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    /**
+     * Register a new User
+     * @param request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function signup(SignUpRequest $request)
+    {
+        User::create($request->all());
+        return $this->login($request);
+    }
+
+    /**
+     * Get the authenticated User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
+
+    /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    /**
+     * Refresh a token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh()
+    {
+        return $this->respondWithToken(auth()->refresh());
+    }
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60,
+            'user' => auth()->user()->email
+        ]);
+    }
+}
